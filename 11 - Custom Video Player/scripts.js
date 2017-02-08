bb054c0d6a7a3498acb4b9df2dc81fea27d3fa26
Hetty de Vries <hetty82@gmail.com>
2017-02-08 19:34:24 +0100

    11 Custom Video Player - finish exercise
    - and add fullscreen button

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,65 @@
+// Get our element nodes
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress__filled')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+const fullscreen = player.querySelector('.toggle-fullscreen')
+
+console.dir(video)
+
+// Build our functions
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause'
+  video[method]()
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚'
+  toggle.textContent = icon
+}
+
+function skip() {
+  const skip = this.dataset.skip
+  video.currentTime += +skip
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+}
+
+function requestFullScreen(e) {
+  video.webkitRequestFullscreen()
+}
+
+// Hook up the event listeners
+video.addEventListener('playing', updateButton)
+video.addEventListener('pause', updateButton)
+
+video.addEventListener('click', togglePlay)
+toggle.addEventListener('click', togglePlay)
+
+video.addEventListener('timeupdate', handleProgress)
+
+let mousedown = false
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', () => mousedown = true)
+progress.addEventListener('mouseup', () => mousedown = false)
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate))
+
+fullscreen.addEventListener('click', requestFullScreen)
